1444f6474d49eb2987fffa6a878e91f7235ca127
klad <kyleladuke@gmail.com>
2014-11-19 21:14:56 -0500

    Cleaned up usings

--- IntelligentDisplayFix/Program.cs
+++ IntelligentDisplayFix/Program.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Microsoft.Win32;
-using System.IO;
+﻿using Microsoft.Win32;
+using System;
 using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
 
 namespace IntelligentDisplayFix
 {
